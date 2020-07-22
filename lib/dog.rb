db1e2accc3dee6a8f28bd1f21bf0b984efaa3a82
Ian Mayton <ian.mayton@gmail.com>
2020-07-22 19:33:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
-# Add your code here
+class Dog 
+  @@dogs = []
+  
+  def initialize(name)
+  @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.print_all
+    @@all.each do |dog| 
+      puts dog 
+    end 
+    
+  def self.clear_all 
+    @@all = [] 
+  end 
+  
+end
